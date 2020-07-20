9763a9667bfdfba274ab32fe1ed78fffb4f27e63
zxy <308885823@qq.com>
2020-07-20 15:42:24 +0800

    初始化项目

---
+++ src/test/java/org/zxy/flea/FleaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zxy.flea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
